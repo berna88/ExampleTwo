ddd045b39fa172e45e16f668e1caee5ee9307155
Bernardo <bhernandez@consistent.com.mx>
2018-05-24 17:31:52 -0500

    Ajuste de portlet en liferay

--- src/main/java/com/exampletwo/portlet/PrincipalPortletViewController.java
+++ src/main/java/com/exampletwo/portlet/PrincipalPortletViewController.java
@@ -2,11 +2,16 @@ package com.exampletwo.portlet;
 
 import java.util.Map;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.ActionMapping;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
 
 /**
@@ -24,9 +29,24 @@ public class PrincipalPortletViewController {
 	
 	@RenderMapping(params = "action=showForm")
 	public String Form(Map<String, Object> map){
-		
+		Persona persona = new Persona();
+		map.put("persona", persona);
 		return FORM;
 	}
+	
+	@RenderMapping(params = "action=success")
+	public String viewSuccess() {
+	    return "success";
+	 
+	}
+	@ActionMapping(value = "handleCustomer")
+    public void getCustomerData(@ModelAttribute("persona") Persona persona, ActionRequest actionRequest, ActionResponse actionResponse, Model model) {
+        actionResponse.setRenderParameter("action", "success");
+ 
+        model.addAttribute("successModel", persona);
+    }
+	
+	
 }
 
 class Persona{
--- src/main/webapp/WEB-INF/jsp/form.jsp
+++ src/main/webapp/WEB-INF/jsp/form.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +9,26 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>hola</h1>
+<portlet:defineObjects />
+<portlet:actionURL var="submitFormURL" name="handleCustomer"/>
+<form:form name="persona" method="post" modelAttribute="persona" action="<%=submitFormURL.toString() %>">
+<br/>
+    <table style="margin-left:80px">
+        <tbody>
+            <tr>
+                <td><form:label path="name">Name</form:label></td>
+                <td><form:input path="name"></form:input></td>
+            </tr>
+            <tr>
+                <td><form:label path="age">Age</form:label></td>
+                <td><form:input path="age"></form:input></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" value="Submit Form">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</form:form>
 </body>
 </html>
